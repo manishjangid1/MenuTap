b5080ca9c992e8f1f49f83a72d7b5dd99bf7a0c3
Manish Jangid <manishjangidinfo@gmail.com>
2026-08-27 00:31:21 +0530

    Fix static files and Cloudinary

--- menutap/settings.py
+++ menutap/settings.py
@@ -128,6 +128,15 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 from pathlib import Path
 
 import cloudinary
