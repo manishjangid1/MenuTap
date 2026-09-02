5f3fb729ba6ba6b44688477f03baad94faefe68d
Manish Jangid <manishjangidinfo@gmail.com>
2026-09-02 21:56:28 +0530

    Fix duplicate settings and set proper storage backends

--- menutap/settings.py
+++ menutap/settings.py
@@ -90,7 +90,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('nvprwlkq', ''),
-    'API_KEY': os.environ.get('287248521182818, ''),
+    'API_KEY': os.environ.get('287248521182818', ''),
     'API_SECRET': os.environ.get('Nzeb_MEwQL60oa6jxVSMv-gwL6E', ''),
 }
 
